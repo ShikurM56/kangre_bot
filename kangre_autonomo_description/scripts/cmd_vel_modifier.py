e794b49f56111e5a62c946f43be258f0c38e5fba
ShikurM56 <marcomoreno.aztech@gmail.com>
2022-05-01 19:32:17 -0500

    Simulation OK

---
+++ kangre_autonomo_description/scripts/cmd_vel_modifier.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import rospy
+from geometry_msgs.msg import Twist
+
+def cmd_vel_modifier():
+    rospy.init_node('cmd_vel_publisher', anonymous=True)
+    rospy.Subscriber("cmd_vel", Twist, callback)
+    rate = rospy.Rate(10) # 10hz
+    rospy.spin()
+
+   
+
+def callback(msg):
+    pub = rospy.Publisher('cmd_vel_2', Twist, queue_size=10)
+
+    newTwist = Twist()
+    newTwist.linear.x = 2*msg.linear.x
+    newTwist.angular.z = msg.angular.z
+    pub.publish(newTwist)
+
+if __name__ == '__main__':
+    try:
+        cmd_vel_modifier()
+    except rospy.ROSInterruptException:
+        pass
